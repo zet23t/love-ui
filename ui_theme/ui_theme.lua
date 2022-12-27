fc42f73c5ec8cee081ef167145bb8e28f6db88df
Eike Decker <zet23t@googlemail.com>
2022-12-27 22:57:04 +0100

    Adding named icons to ui_theme + helper functions

--- ui_theme/ui_theme.lua
+++ ui_theme/ui_theme.lua
@@ -1,7 +1,42 @@
+---@class ui_theme : object
 local ui_theme = require "love-util.class" "ui_theme"
 
 local sprite9_component = require "love-ui.components.generic.sprite9_component"
+local sprite_component = require "love-ui.components.generic.sprite_component"
 local text_component = require "love-ui.components.generic.text_component"
+
+ui_theme.icon = {
+	cursor = 1;
+	open_folder = 5;
+	save_disk = 6;
+	close_x = 7;
+	toggle_not_set = 10;
+	toggle_set = 11;
+	send_up = 15;
+	home = 16;
+	cursor_resize = 17;
+	move = 18;
+	rotate = 19;
+	vertice_add = 20;
+	vertice_remove = 21;
+	fill = 22;
+	diagonal_sash = 23;
+	selection = 25;
+	undo = 26;
+	redo = 27;
+	select_all = 28;
+	select_none = 29;
+	select_invert = 30;
+	send_down = 31;
+	scale = 32;
+	tiny_triangle_left = 33;
+	tiny_triangle_right = 34;
+	tiny_triangle_up = 35;
+	tiny_triangle_down = 36;
+	play = 37;
+	pause = 38;
+	to_end = 39;
+}
 
 function ui_theme:new()
 	return ui_theme:create()
@@ -47,6 +82,20 @@ function ui_theme:decorate_button_skin(ui_rect, caption, on_click)
 			end
 		})
 	end
+end
+
+function ui_theme:decorate_on_click(ui_rect, on_click)
+	ui_rect:add_component({
+		was_released = function(self, rect)
+			if rect.flags.is_mouse_over then
+				on_click()
+			end
+		end
+	})
+end
+
+function ui_theme:decorate_sprite(ui_rect, icon_id)
+	ui_rect:add_component(sprite_component:new(icon_id))
 end
 
 function ui_theme:window_skin()
