eb189cbb7cb9382f8630dc1256aec6f4c4c8fe03
Eike Decker <zet23t@googlemail.com>
2022-12-28 10:27:24 +0100

    displaying now the directories

--- widget/file_dialog_widget.lua
+++ widget/file_dialog_widget.lua
@@ -1,3 +1,4 @@
+local nativefs                      = require "nativefs"
 local ui_rect                       = require "love-ui.ui_rect"
 local parent_size_matcher_component = require "love-ui.components.layout.parent_size_matcher_component"
 local rectfill_component            = require "love-ui.components.generic.rectfill_component"
@@ -5,13 +6,17 @@ local weighted_position_component   = require "love-ui.components.layout.weighte
 local parent_limited_size_component = require "love-ui.components.layout.parent_limited_size_component"
 local text_component                = require "love-ui.components.generic.text_component"
 local clip_component                = require "love-ui.components.generic.clip_component"
+local pico8api                      = require "love-ui.pico8api"
+local clip_stack                    = require "love-ui.clip_stack"
+local scrollbar_component           = require "love-ui.components.generic.scrollbar_component"
 
 ---@class file_dialog_widget : object
 ---@field screen_rect ui_rect the full screen rect
+---@field directory_hierarchy_list ui_rect
 local file_dialog_widget = require "love-util.class" "file_dialog_widget"
 
 ---@param ui_theme ui_theme
----@return object
+---@return file_dialog_widget
 function file_dialog_widget:new(ui_theme)
 	---@type file_dialog_widget
 	---@diagnostic disable-next-line: assign-type-mismatch
@@ -53,9 +58,37 @@ function file_dialog_widget:new(ui_theme)
 		}
 	)
 
-	self.directory_hierarchy_clip_area = ui_rect:new(0,0,0,0, self.directory_hierarchy,
-		parent_size_matcher_component:new(1,1,1,1))
-	self.directory_hierarchy_list = ui_rect:new(0,0,0,0)
+	self.directory_hierarchy_scroll = ui_rect:new(0, 0, 8, 100, self.directory_hierarchy,
+		weighted_position_component:new(1, 0),
+		parent_size_matcher_component:new(0, true, 0, true),
+		scrollbar_component:new_themed(2, ui_theme))
+
+	self.directory_hierarchy_clip_area = ui_rect:new(0, 0, 0, 0, self.directory_hierarchy,
+		parent_size_matcher_component:new(1, 8, 1, 1),
+		clip_component:new()
+	)
+	local line_height = 9
+	self.directory_hierarchy_list = ui_rect:new(0, 0, 0, 0, self.directory_hierarchy_clip_area, {
+		layout_update_size = function(comp, ui_rect)
+			ui_rect.h = #self.directory_hierarchy_list * line_height
+			ui_rect.w = ui_rect.parent.w
+		end;
+		draw = function(comp, ui_rect)
+			-- love.graphics.setScissor()
+			local x, y = ui_rect:to_world()
+			local cx1, cy1, cw, ch = clip_stack:current_rect()
+			local cy2 = cy1 + ch
+			local start = math.max(1, math.floor((y - cy1) / line_height))
+			for i = start, math.min(#self.directory_hierarchy_list, math.ceil(start + ch / line_height) - 1) do
+				local dir_info = self.directory_hierarchy_list[i]
+				local px = x + 4 * dir_info.level
+				local py = y + (i - 1) * line_height
+				pico8api:spr(dir_info.is_opened and ui_theme.icon.open_folder or ui_theme.icon.closed_folder, px, py)
+				pico8api:print(dir_info.file_name, px + 10, py + 2, 1)
+			end
+		end
+	})
+
 
 	self:set_directory(love.filesystem.getWorkingDirectory())
 
@@ -63,7 +96,32 @@ function file_dialog_widget:new(ui_theme)
 end
 
 function file_dialog_widget:set_directory(path)
+	local current_path = ""
 
+	self.directory_hierarchy_list = {}
+	local function amend(current_path, level)
+		level = level + 1
+		local files = nativefs.getDirectoryItems(current_path)
+		table.sort(files, function(a, b) return a:lower() < b:lower() end)
+		for i = 1, #files do
+			local file_path = current_path .. files[i]
+			if nativefs.getInfo(file_path, "directory") then
+				local is_opened = file_path == path:sub(1, #file_path)
+				self.directory_hierarchy_list[#self.directory_hierarchy_list + 1] = {
+					level = level;
+					is_opened = is_opened;
+					file_path = file_path;
+					file_name = files[i];
+					directory = current_path;
+				}
+				if is_opened then
+					amend(file_path .. "/", level + 1)
+				end
+			end
+		end
+	end
+
+	amend(path:match "[^/]+/", 0)
 end
 
 function file_dialog_widget:close()
