414bc0080e99a7dceae81a7d73cfaedca23b0586
Eike Decker <zet23t@googlemail.com>
2023-01-06 10:53:12 +0100

    adding docs

--- components/generic/ui_rect_component.lua
+++ components/generic/ui_rect_component.lua
@@ -1,5 +1,5 @@
 ---@class ui_rect_component : object
----@field disabled boolean
+---@field disabled boolean if true, the component is ignored; using disabled instead of enabled because disabled = false for empty tables
 local ui_rect_component = require "love-util.class" "ui_rect_component"
 
 function ui_rect_component:set_enabled(is_enabled)
