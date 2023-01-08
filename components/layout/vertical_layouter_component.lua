44aeba841caf99e445958c2254d327de1188e8db
Eike Decker <zet23t@googlemail.com>
2023-01-08 23:31:50 +0100

    making vertical layouter component a bit more versatile

--- components/layout/vertical_layouter_component.lua
+++ components/layout/vertical_layouter_component.lua
@@ -1,8 +1,13 @@
 ---@class vertical_layouter_component : ui_rect_component
 local vertical_layouter_component = require "love-util.class" "vertical_layouter_component":extends(require "love-ui.components.generic.ui_rect_component")
 
-function vertical_layouter_component:new(donot_update_size)
-	local instance = self:create {}
+function vertical_layouter_component:new(donot_update_size, t, r, b, l)
+	local instance = self:create {
+		t = t or 0;
+		b = b or 0;
+		r = r or 0;
+		l = l or 0;
+	}
 	if donot_update_size then
 		instance.layout_update_size = function() end
 	end
@@ -11,8 +16,8 @@ end
 
 ---@param rect ui_rect
 function vertical_layouter_component:layout_update_size(rect)
-	local h = 0
-	local w = 0
+	local h = self.t + self.b
+	local w = self.l + self.r
 	for i = 1, #rect.children do
 		local child = rect.children[i]
 		h = h + child.h
@@ -23,10 +28,10 @@ end
 
 ---@param rect ui_rect
 function vertical_layouter_component:layout_update(rect)
-	local pos = 0
+	local pos = self.t
 	for i = 1, #rect.children do
 		local child = rect.children[i]
-		child.x = math.floor((rect.w - child.w) / 2)
+		child.x = math.floor((rect.w - self.r - self.l - child.w) / 2)
 		child.y = pos
 		pos = pos + child.h
 	end
