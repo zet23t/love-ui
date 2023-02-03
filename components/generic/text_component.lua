56cb98649cff25a69b89bb6158c22911d92c4398
Eike Decker <zet23t@googlemail.com>
2023-02-03 23:12:08 +0100

    multiline support for text component

--- components/generic/text_component.lua
+++ components/generic/text_component.lua
@@ -10,6 +10,10 @@ local pico8api = require "love-ui.pico8api"
 ---@field b number
 ---@field l number
 local text_component = require "love-util.class" "text_component":extends(require "love-ui.components.generic.ui_rect_component")
+text_component.line_height = 12
+text_component.line_spacing = 2
+text_component.newline_indent = 0
+text_component.firstline_indent = 0
 
 function text_component:new(text, color, t, r, b, l, align_x, align_y)
 	return text_component:create {
@@ -21,12 +25,60 @@ function text_component:new(text, color, t, r, b, l, align_x, align_y)
 	}
 end
 
+function text_component:set_newline_indent(x)
+	self.newline_indent = x
+	return self
+end
+
+function text_component:set_firstline_indent(x)
+	self.firstline_indent = x
+	return self
+end
+
+function text_component:set_multiline(enabled)
+	self.is_multiline_enabled = enabled
+	return self
+end
+
+local function get_wrapped_text(self, text, max_width)
+	local line = ""
+	local line_width = 0
+	local lines = {}
+	for fragment, word in text:gmatch "(%s*(%S+))" do
+		local width = pico8api:text_width(fragment)
+		if line_width + width > max_width and line_width > 0 then
+			lines[#lines + 1] = { line, line_width }
+			line = word
+			line_width = pico8api:text_width(line) + self.newline_indent
+		else
+			line_width = line_width + width
+			line = line .. fragment
+		end
+	end
+	if #line > 0 then
+		lines[#lines + 1] = { line, line_width }
+	end
+	return lines
+end
+
+local function layout_update_size(self, rect)
+	local maxpos_x = rect.w - self.r - self.l
+	local lines = get_wrapped_text(self, self.text, maxpos_x)
+	rect.h = self.line_height * #lines + self.line_spacing * (#lines - 1) + self.b + self.t
+end
+
+function text_component:set_fitting_height(enabled)
+	self.is_fitting_height = enabled
+	self.layout_update_size = enabled and layout_update_size
+	return self
+end
+
 function text_component:set_rotation(rotation)
 	self.rotation = rotation or self.rotation
 	return self
 end
 
-local function rotate(rotation, x0,y0, x, y, ...)
+local function rotate(rotation, x0, y0, x, y, ...)
 	local dx, dy = x - x0, y - y0
 	local s, c = math.sin(rotation), math.cos(rotation)
 	dx, dy = s * dy + c * dx, s * dx + c * dy
@@ -37,27 +89,44 @@ local function rotate(rotation, x0,y0, x, y, ...)
 end
 
 function text_component:draw(ui_rect)
+	local t, r, b, l = self.t, self.r, self.b, self.l
 	local x0, y0 = ui_rect:to_world()
 	local w = pico8api:text_width(self.text)
-	local t, r, b, l = self.t, self.r, self.b, self.l
+	local h = self.line_height
 	local maxpos_x = ui_rect.w - r - l
 	local maxpos_y = ui_rect.h - t - b
-	local x = x0 + l + self.align_x * maxpos_x - w * self.align_x
-	local y = y0 + t + self.align_y * maxpos_y - 12 * self.align_y + 1
 
-	local min_x, min_y, max_x, max_y = x0 + l,
-		y0 + t,
-		x0 + ui_rect.w - r, y0 + ui_rect.h - b
-	-- pico8api:rect(min_x, min_y, max_x, max_y,1)
-	-- pico8api:rect(x, y, w + x, y + 16,1)
-	-- pico8api:rect(x0, y0, ui_rect.w + x0, y0 + ui_rect.h,2)
-	if self.rotation ~= 0 then
-		x,y, min_x, min_y, max_x, max_y = rotate(self.rotation, x0, y0, x,y, min_x, min_y, max_x, max_y)
-		-- min_x, min_y = rotate(self.rotation, x,y,x0,y0)
-		-- pico8api:rect(x,y,max_x, max_y, 1)
-		
+	local function print_line(text, w, xoff, yoff)
+		local x = x0 + l + self.align_x * maxpos_x - w * self.align_x
+		local y = y0 + t + self.align_y * maxpos_y - h * self.align_y + 1
+
+		local min_x, min_y, max_x, max_y = x0 + l,
+			y0 + t,
+			x0 + ui_rect.w - r, y0 + ui_rect.h - b
+		-- pico8api:rect(min_x, min_y, max_x, max_y,1)
+		-- pico8api:rect(x, y, w + x, y + 16,1)
+		-- pico8api:rect(x0, y0, ui_rect.w + x0, y0 + ui_rect.h,2)
+		if self.rotation ~= 0 then
+			x, y, min_x, min_y, max_x, max_y = rotate(self.rotation, x0, y0, x, y, min_x, min_y, max_x, max_y)
+			-- min_x, min_y = rotate(self.rotation, x,y,x0,y0)
+			-- pico8api:rect(x,y,max_x, max_y, 1)
+
+		end
+		-- pico8api:rect(min_x, min_y, max_x, max_y, 1)
+		pico8api:print(text, x + xoff, y + yoff, self.color, min_x, min_y, max_x, max_y, self.rotation)
 	end
-	pico8api:print(self.text, x, y, self.color, min_x, min_y, max_x, max_y, self.rotation)
+
+	if w > maxpos_x and self.is_multiline_enabled then
+		local lines = get_wrapped_text(self, self.text, maxpos_x)
+		local line_offset = self.line_height + self.line_spacing
+		h = self.line_height * #lines + self.line_spacing * (#lines - 1)
+		for i,line in ipairs(lines) do 
+			print_line(line[1], line[2], i > 1 and self.newline_indent or self.firstline_indent, (i - 1) * line_offset)
+		end
+		-- print(self.text)
+	else
+		print_line(self.text, w, 0, 0)
+	end
 end
 
 function text_component:set_text(text)
