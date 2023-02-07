793cc595416a36b24487d682d962020e11d0746d
Eike Decker <zet23t@googlemail.com>
2023-02-07 22:27:30 +0100

    a layouter that can do horizontal + vertical layouting

---
+++ components/layout/linear_layouter_component.lua
@@ -0,0 +1,88 @@
+---@class linear_layouter_component : ui_rect_component
+local linear_layouter_component = require "love-util.class" "linear_layouter_component":extends(require "love-ui.components.generic.ui_rect_component")
+
+---@param donot_update_size boolean|nil
+---@param axis 1|2
+---@param t number|nil
+---@param r number|nil
+---@param b number|nil
+---@param l number|nil
+---@return linear_layouter_component
+function linear_layouter_component:new(axis, donot_update_size, t, r, b, l, spacing)
+	local instance = self:create {
+		axis = axis or 1,
+		t = t or 0;
+		b = b or 0;
+		r = r or 0;
+		l = l or 0;
+		spacing = spacing or 0;
+	}
+	if donot_update_size then
+		instance.layout_update_size = function() end
+	end
+	return instance
+end
+
+function linear_layouter_component:set_minor_axis_fit_enabled(enabled)
+	self.minor_axis_fit_enabled = enabled
+	return self
+end
+
+function linear_layouter_component:set_minor_axis_expand_children_enabled(enabled)
+	self.minor_axis_expand_children_enabled = enabled
+	return self
+end
+
+---@param rect ui_rect
+function linear_layouter_component:layout_update_size(rect)
+	local h = self.t + self.b
+	local w = self.l + self.r
+	local major_val, minor_val, major_key, minor_key = w, h, "w", "h"
+	if self.axis == 2 then
+		major_val, minor_val = minor_val, major_val
+		major_key, minor_key = minor_key, major_key
+	end
+
+	local max_minor_val = minor_val
+
+	for i = 1, #rect.children do
+		local child = rect.children[i]
+		if not child.ignore_layouting then
+			if self.minor_axis_expand_children_enabled then
+				child[minor_key] = rect[minor_key] - minor_val
+			elseif self.minor_axis_fit_enabled then
+				max_minor_val = math.max(max_minor_val, child[minor_key] + minor_val)
+			end
+			child:do_layout_size_update()
+			major_val = major_val + child[major_key] + (i > 1 and self.spacing or 0)
+		end
+	end
+
+	rect[major_key] = major_val
+	if self.minor_axis_fit_enabled then
+		rect[minor_key] = max_minor_val
+	end
+end
+
+---@param rect ui_rect
+function linear_layouter_component:layout_update(rect)
+	local pos = self.l
+	local major_a, major_b = self.t, self.b
+	local major_pos, minor_pos = "y", "x"
+	local major_size, minor_size = "w", "h"
+	if self.axis == 2 then
+		pos = self.t
+		major_a, major_b = self.l, self.r
+		major_pos, minor_pos = "x", "y"
+		major_size, minor_size = "h", "w"
+	end
+
+	for i = 1, #rect.children do
+		local child = rect.children[i]
+		child[major_pos] = math.floor((rect[minor_size] - major_b - major_a - child[minor_size]) / 2 + major_a)
+		child[minor_pos] = pos
+		pos = pos + child[major_size] + self.spacing
+	end
+end
+
+return linear_layouter_component
