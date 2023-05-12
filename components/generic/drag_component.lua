ae59c151d75d655d039b7e709ceed09ac41243c2
Eike Decker <zet23t@googlemail.com>
2023-05-12 05:23:47 +0200

    More functionality for drag component
    now with callbacks for begin / end of dragging

--- components/generic/drag_component.lua
+++ components/generic/drag_component.lua
@@ -8,11 +8,13 @@ local function donothing() end
 ---@param dragged_rect ui_rect|nil the ui_rect to drag; if nil, the component will move its own rect
 ---@param raster number|nil the raster to use to align the rect. Defaults to 1.
 ---@return unknown
-function drag_component:new(dragged_rect, raster, on_position_updated)
+function drag_component:new(dragged_rect, raster, on_position_updated, on_begin, on_finished)
 	return drag_component:create {
 		dragged_rect = dragged_rect;
 		raster = raster or 1;
 		on_position_updated = on_position_updated;
+		on_begin = on_begin;
+		on_finished = on_finished;
 	}
 end
 
@@ -21,16 +23,30 @@ end
 
 function drag_component:is_pressed_down(rect, mx, my)
 	rect = self.dragged_rect or rect
+	local px,py = rect.x, rect.y
 	local rx = math.floor((rect.x + mx - self.mx) / self.raster + .5) * self.raster
 	local ry = math.floor((rect.y + my - self.my) / self.raster + .5) * self.raster
 	local x,y = self:on_position_updated(rect, rx, ry)
 	rect.x = x or rx
 	rect.y = y or ry
+	local dx,dy = rect.x - px, rect.y - py
+	self.distance = self.distance + (dx*dx+dy*dy)^.5
+end
+
+function drag_component:was_released()
+	if self.on_finished and self.distance > 0 then
+		self:on_finished()
+	end
 end
 
 function drag_component:was_pressed_down(ui_rect, mx, my)
 	if ui_rect.flags.is_top_hit then
 		self.mx, self.my = mx, my
+		self.distance = 0
+		if self.on_begin then
+			self:on_begin()
+		end
+		
 		self.is_pressed_down = nil
 	else
 		self.is_pressed_down = donothing
